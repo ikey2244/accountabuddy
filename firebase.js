41036ce2875a9c88671516c862eaecbda32203cc
ikey2244 <ikey2244@gmail.com>
2019-05-22 21:52:54 -0500

    Add env variables

--- firebase.js
+++ firebase.js
@@ -1,9 +1,11 @@
+import { FIREBASE_API_KEY } from 'react-native-dotenv';
+
 export const firebaseConfig = {
-   apiKey: "AIzaSyBwR-nXXxyofxjYcsTjmID_CW3LO-5MbNs",
-   authDomain: "accountabuddy-134b8.firebaseapp.com",
-   databaseURL: "https://accountabuddy-134b8.firebaseio.com",
-   projectId: "accountabuddy-134b8",
-   storageBucket: "accountabuddy-134b8.appspot.com",
-   messagingSenderId: "888670357488",
-   appId: "1:888670357488:web:76cb048ae97457c8"
- };
+  apiKey: FIREBASE_API_KEY,
+  authDomain: 'accountabuddy-134b8.firebaseapp.com',
+  databaseURL: 'https://accountabuddy-134b8.firebaseio.com',
+  projectId: 'accountabuddy-134b8',
+  storageBucket: 'accountabuddy-134b8.appspot.com',
+  messagingSenderId: '888670357488',
+  appId: '1:888670357488:web:76cb048ae97457c8',
+};
