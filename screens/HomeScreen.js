4e9a4fcf0008ec2676f317bb5614a4d065542f06
ikey2244 <ikey2244@gmail.com>
2019-06-06 08:13:15 -0500

    Add theme to calender

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,31 +1,62 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { CalendarList } from 'react-native-calendars';
+import { StyleSheet, Text, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: 'Home',
+    header: null,
   };
 
   render() {
+    const currentDate = new Date().toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
     return (
-      <CalendarList
-        // Enable horizontal scrolling, default = false
-        horizontal
-        // Enable paging on horizontal, default = false
-        pagingEnabled
-        // Set custom calendarWidth.
-        calendarWidth={320}
-      />
+      <View styles={styles.container}>
+        <Text style={styles.headline}>{currentDate}</Text>
+        <Calendar
+          // Specify style for calendar container element. Default = {}
+          // Specify theme properties to override specific styles for calendar parts. Default = {}
+          theme={{
+            backgroundColor: '#ffffff',
+            calendarBackground: '#ffffff',
+            textSectionTitleColor: '#b6c1cd',
+            selectedDayBackgroundColor: '#00adf5',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#00adf5',
+            dayTextColor: '#2d4150',
+            textDisabledColor: '#d9e1e8',
+            dotColor: '#00adf5',
+            selectedDotColor: '#ffffff',
+            arrowColor: 'orange',
+            monthTextColor: 'blue',
+            textDayFontWeight: '300',
+            textMonthFontWeight: 'bold',
+            textDayHeaderFontWeight: '300',
+            textDayFontSize: 16,
+            textMonthFontSize: 16,
+            textDayHeaderFontSize: 16,
+          }}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
     flex: 1,
     backgroundColor: '#fff',
+  },
+  headline: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 0,
+    textAlign: 'center',
   },
 });
